bab02603d08135e1f15c4b6375b577a8d9564beb
Leandro Arturi <lea.arturi@gmail.com>
2023-01-10 13:44:43 -0300

    Map

---
+++ server/03-map-reloaded.js
@@ -0,0 +1,34 @@
+const orders = [
+   {
+      customerName: 'Nicolas',
+      total: 60,
+      delivered: true,
+   },
+   {
+      customerName: 'Zulema',
+      total: 120,
+      delivered: false,
+   },
+   {
+      customerName: 'Santiago',
+      total: 180,
+      delivered: true,
+   },
+   {
+      customerName: 'Valentina',
+      total: 240,
+      delivered: true,
+   },
+];
+
+const newArray = orders.map((item) => item.total);
+
+const newArray2 = orders.map((item) => {
+   return {
+      ...item,
+      tax: 0.21 * item.total,
+   };
+});
+
+console.log(orders);
+console.log(newArray2);
