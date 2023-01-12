6a08496c3fb7b0bc96d2fc221eaf3fd719578557
Leandro Arturi <lea.arturi@gmail.com>
2023-01-12 19:49:04 -0300

    Join

---
+++ server/10-join.js
@@ -0,0 +1,9 @@
+const elements = ['fire', 'air', 'water'];
+
+const rta1 = elements.join(', ');
+console.log({ rta1 });
+
+// Urls amigables SEO
+const title = 'Curso de manipulacion de arrays con js';
+rta2 = title.split(' ').join('-').toLowerCase();
+console.log({ rta2 });
